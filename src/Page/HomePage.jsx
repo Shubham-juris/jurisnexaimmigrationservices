3d849613629444c0c4bad7d43c152b7b1a4a90c8
sumit0079-tech <sumit.jurisnexa03@gmail.com>
2025-04-25 12:14:32 +0530

    main

--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -1,22 +1,23 @@
 import About from "../components/Home/About";
 import Hero from "../components/Home/Hero";
 import HomeBlog from "../components/Home/HomeBlog";
-// import LocationsSlider from "../components/Home/LocationsSlider";
 import Media from "../components/Home/Media";
 import Services from "../components/Home/Service";
+import Country from "../components/Home/Country";
+import ClientReviews from "../components/Home/ClientReviews";
 
 
-const HomePage = () => {
+export default function HomePage ()  {
   return (
-    <div>
+    <>
       <Hero />
       <Services />
       <About />
       <HomeBlog />
-      {/* <LocationsSlider /> */}
+      <Country /> 
+      <ClientReviews />
       <Media />
-    </div>
-  );
-};
+    </>
+  )
+}
 
-export default HomePage;
